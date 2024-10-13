418da42375f27c083f383a9f1f5bbbc938b2b09b
mraibel <mraibel12@gmail.com>
2024-10-13 20:26:03 -0300

    first commit

---
+++ src/main/java/sdp/api/PoloSaludApplication.java
@@ -0,0 +1,13 @@
+package sdp.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoloSaludApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoloSaludApplication.class, args);
+	}
+
+}
